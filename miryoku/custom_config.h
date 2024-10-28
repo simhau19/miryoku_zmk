6cd33ac0e4669d35ca95cf07917658239ce6b8a2
Simen Haug <simehaug@stud.ntnu.no>
2024-10-28 12:31:00 +0100

    added æøå using international us keyboard layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -14,9 +14,9 @@
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
 &kp GRAVE  &kp N1  &kp N2  &kp N3  &kp N4  &kp N5                    &kp N6  &kp N7  &kp N8  &kp N9  &kp N0  &kp SEMI \
-&kp TAB    K00     K01     K02     K03     K04                       K05     K06     K07     K08     K09     &kp F15 \
-&kp LCTRL  K10     K11     K12     K13     K14                       K15     K16     K17     K18     K19     &kp F13 \
-&kp LSHFT  K20     K21     K22     K23     K24    &kp LBKT &kp RBKT  K25     K26     K27     K28     K29     &kp F14 \
+&kp TAB    K00     K01     K02     K03     K04                       K05     K06     K07     K08     K09     &kp RA(W) \
+&kp LCTRL  K10     K11     K12     K13     K14                       K15     K16     K17     K18     K19     &kp RA(Z) \
+&kp LSHFT  K20     K21     K22     K23     K24    &kp LBKT &kp RBKT  K25     K26     K27     K28     K29     &kp RA(L) \
                    XXX     K32     K33     K34                       K35     K36     K37     XXX
 
 #endif
