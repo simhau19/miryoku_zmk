c17e52d2886513c1c3036cc0695893bc75ef4b91
Simen Haug <simehaug@stud.ntnu.no>
2024-11-03 21:36:54 +0100

    updated ø and special character placement in alpha layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -13,10 +13,10 @@
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-&kp GRAVE  &kp N1  &kp N2  &kp N3  &kp N4  &kp N5                    &kp N6  &kp N7  &kp N8  &kp N9  &kp N0  &kp SEMI \
-&kp TAB    K00     K01     K02     K03     K04                       K05     K06     K07     K08     K09     &kp RA(W) \
-&kp LCTRL  K10     K11     K12     K13     K14                       K15     K16     K17     K18     K19     &kp RA(Z) \
-&kp LSHFT  K20     K21     K22     K23     K24    &kp LBKT &kp RBKT  K25     K26     K27     K28     K29     &kp RA(L) \
+&kp GRAVE  &kp N1  &kp N2  &kp N3  &kp N4  &kp N5                    &kp N6  &kp N7  &kp N8  &kp N9  &kp N0  &kp EQUAL \
+&kp SEMI   K00     K01     K02     K03     K04                       K05     K06     K07     K08     K09     &kp RA(W) \
+&kp RA(L)  K10     K11     K12     K13     K14                       K15     K16     K17     K18     K19     &kp RA(Z) \
+&kp MINUS  K20     K21     K22     K23     K24    &kp LBKT &kp RBKT  K25     K26     K27     K28     K29     &kp BSLH \
                    XXX     K32     K33     K34                       K35     K36     K37     XXX
 
 #endif
